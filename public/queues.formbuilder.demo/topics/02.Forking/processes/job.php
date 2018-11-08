d65d43156a587e7b0ca0adf1f06ee07e11985f0b
Alexandru Berce <alexandru.berce@gmail.com>
2018-11-09 00:53:28 +0200

    Update job file

--- public/queues.formbuilder.demo/topics/02.Forking/processes/job.php
+++ public/queues.formbuilder.demo/topics/02.Forking/processes/job.php
@@ -2,10 +2,15 @@
 
 /** @var int $childId */
 
+if(!isset($childId))
+	$childId = null;
+
+$jobName = isset($jobName) ? "[$jobName]" : '';
+
 $secondsTotal = $secondsLeft = rand(4, 15);
 echo "[W$childId] Doing my job, it's taking $secondsTotal seconds... \r\n";
 
 while ($secondsLeft--){
-	echo "[W$childId] Job progress [$secondsLeft of $secondsTotal]... \r\n";
+	echo "[W$childId] Job progress $jobName [$secondsLeft of $secondsTotal]... \r\n";
 	sleep(1);
 }
